3a69c7243919fc2e32b80d9c1a5d33d6774bcaaf
Cynthia Gainey <cynthiagainey@ymail.com>
2022-06-01 15:37:49 -0400

    Added UI elements
    Added Pause menu. Generated Input binding so menu is using the new input system

--- KibblesInSpace/Assets/Scripts/Enemy/EnemyMovement.cs
+++ KibblesInSpace/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,8 +6,11 @@ public class EnemyMovement : MonoBehaviour
     // For Waypoint movement
     [SerializeField] float _speed;
     [SerializeField] float _waitTime;
-
+    
+    [Tooltip("How far till the Enemy sees the player")]
     [SerializeField] int _radius;
+
+    [Tooltip("Players Transform so enemy will follow players location")]
     [SerializeField] Transform _playerTransform;
     [Tooltip("Path the Enemy will follow")]
     [SerializeField] Transform[] _waypoints;
--- KibblesInSpace/Assets/Scripts/LevelManager.cs
+++ KibblesInSpace/Assets/Scripts/LevelManager.cs
@@ -1,24 +1,89 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class LevelManager : MonoBehaviour
 {
+    // This might not work as i intend but want to test
+    [Tooltip("String needs to match level name in editor")]
     [SerializeField] const string MAIN_MENU_NAME = "MainMenu";
     [SerializeField] const string LEVEL_ONE_NAME = "LevelOne";
 
+    [Tooltip("This is the Pause Menu Canvas")]
+    [SerializeField] GameObject pauseMenu;
 
+    bool isPaused = false;
+
+    KibblesInSpace _controls;
+
+    
+    private void Awake()
+    {
+        _controls = new KibblesInSpace();
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+
+    private void Start()
+    {
+        //Event binding to function. '_' means we arent passing any parameters to function
+        _controls.UI.PauseMenu.performed += _ => DetermineIfPaused();
+    }
+
+    #region Menu Buttons
     public void MainMenu()
     {
         SceneManager.LoadScene(MAIN_MENU_NAME);
     }
 
+    void DetermineIfPaused()
+    {
+        if (isPaused) 
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        //if in editor will close out of play mode else will quit
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+    #endregion Menu Buttons
+
+    #region Levels
     public void LoadLevelOne()
     {
         SceneManager.LoadScene(LEVEL_ONE_NAME);
     }
 
-    public void Quit()
-    {
-        Application.Quit();
-    }
+    #endregion Levels
+
 }
